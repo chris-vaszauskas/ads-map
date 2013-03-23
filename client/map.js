d6b8d988ea8fe7d5f94782079d6ccbc5e873fd7d
Chris Vaszauskas <chris.vaszauskas@gmail.com>
2013-03-23 12:40:43 -0500

    Drop a marker on long-press

--- client/map.js
+++ client/map.js
@@ -9,4 +9,26 @@ function initializeMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  // Whenever a long press occurs on the map, drop a marker in the
+  // position where the long press occurred
+  var longpressTimeout;
+  google.maps.event.addListener(map, 'mousedown', function (event) {
+    longpressTimeout = Meteor.setTimeout(function () {
+      var marker = new google.maps.Marker({
+        position: event.latLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: 'Chris'
+      });
+    }, 1000);  // a long press occurs after 1 second
+  });
+
+  // If the mouse is released or moved, the long press is cancelled
+  google.maps.event.addListener(map, 'mouseup', function () {
+    Meteor.clearTimeout(longpressTimeout);
+  });
+  google.maps.event.addListener(map, 'mousemove', function () {
+    Meteor.clearTimeout(longpressTimeout);
+  });
 }
