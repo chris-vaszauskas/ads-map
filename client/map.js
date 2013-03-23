bd41537ad3a841c4c081d518f6827e56e8afc8de
Chris Vaszauskas <chris.vaszauskas@gmail.com>
2013-03-23 12:25:57 -0500

    Initial page layout

---
+++ client/map.js
@@ -0,0 +1,12 @@
+Meteor.startup(function () {
+  initializeMap();
+});
+
+function initializeMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
